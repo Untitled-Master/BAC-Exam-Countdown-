e1742f47d12649903dcc4808a3a1e52467469704
Untitled Master <128633214+Untitled-Master@users.noreply.github.com>
2023-06-10 22:23:01 +0100

    Update main.js

--- main.js
+++ main.js
@@ -23,6 +23,6 @@ var x = setInterval(function() {
   // If the count down is over, write some text 
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+    document.getElementById("demo").innerHTML = "Time is up BOZO";
   }
 }, 1000);
