35cdb85e6d867bd272b7c99078a488f3b7cce136
Untitled Master <128633214+Untitled-Master@users.noreply.github.com>
2023-09-07 00:29:31 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Jun 11, 2023 08:00:00").getTime();
+var countDownDate = new Date("Jun 12, 2023 08:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
